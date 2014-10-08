bdcc3043612784d84aadea1969adfbeb789e3729
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2014-10-08 10:00:01 +0200

    Add libraries needed for Ruby

--- site-cookbooks/vps-rails/attributes/default.rb
+++ site-cookbooks/vps-rails/attributes/default.rb
@@ -1,4 +1,7 @@
-default.packages = %w{ vim emacs git build-essential }
+default.packages = %w{ vim emacs git build-essential zlib1g-dev libssl-dev
+ libreadline-dev libyaml-dev libcurl4-openssl-dev curl libffi-dev autoconf
+ libreadline6 libxml2-dev libxslt1-dev openssl htop
+ libpcre3 libpcre3-dev redis-server memcached }
 
 default.authorization.sudo.users = ["deployer"]
 default.authorization.sudo.passwordless = true
